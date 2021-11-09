f753c48f7d1bd4aee668792bea19d80cfe7248ac
FHU-yezi <yehaowei20060411@qq.com>
2021-11-10 06:37:27 +0800

    feat：增加文章下载工具

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from os import remove
+
 import plotly.graph_objs as go
+from JianshuResearchTools.article import (GetArticleAuthorName,
+                                          GetArticleMarkdown, GetArticleText,
+                                          GetArticleTitle)
 from JianshuResearchTools.assert_funcs import (AssertArticleUrl,
                                                AssertCollectionUrl,
                                                AssertJianshuUrl,
@@ -12,14 +17,14 @@ from JianshuResearchTools.convert import (ArticleUrlToArticleUrlScheme,
 from JianshuResearchTools.exceptions import InputError, ResourceError
 from JianshuResearchTools.user import (GetUserAssetsCount, GetUserFPCount,
                                        GetUserFTNCount, GetUserName)
+from pywebio.session import download
 from pywebio import pin, start_server
 from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import go_app
 from qrcode import make as make_qrcode
-from os import remove
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 host = "120.27.239.120"
 port = "8602"
 
@@ -118,6 +123,48 @@ def URLSchemeCoverter():
     pin.put_input("url", label="网页端 URL", type=TEXT)
     put_button("转换", onclick=Convert)
 
+def ArticleDownloader():
+    """简书小工具集：文章下载工具"""
+    def download_content(format):
+        if format not in ["txt", "markdown"]:
+            toast("格式无效，请检查", color="error")
+            return  # 发生错误，不再运行后续逻辑
+        if "我已阅读以上提示并将合规使用文章内容" not in pin.pin["warning"]:
+            toast("请先勾选免责信息", color="error")
+            return # 用户未勾选免责，不再运行后续逻辑
+        try:
+            AssertArticleUrl(pin.pin["url"])
+        except InputError:
+            toast("输入的 URL 无效，请检查")
+            return # 发生错误，不再运行后续逻辑
+        if format == "txt":
+            filename = GetArticleTitle(pin.pin["url"]) + "_" + GetArticleAuthorName(pin.pin["url"]) + ".txt"
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(GetArticleText(pin.pin["url"]))
+        elif format == "markdown":
+            filename = GetArticleTitle(pin.pin["url"]) + "_" + GetArticleAuthorName(pin.pin["url"]) + ".md"
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(GetArticleMarkdown(pin.pin["url"]))
+        
+        toast("文章内容获取成功", color="success")
+        with open(filename, "rb") as f:
+            download(filename, f.read())  # 向浏览器发送文件下载请求
+        
+        remove(filename)  # 删除临时文件
+        
+    
+    put_markdown("""
+    # 文章下载工具
+    本工具可下载简书中的文章内容，并将其保存至本地。
+    
+    **请注意：本工具可以下载设置为禁止转载的文章内容，您需自行承担不规范使用可能造成的版权风险。**
+    """, lstrip=True)
+    
+    pin.put_input("url", type=TEXT, label="要下载的文章链接")
+    pin.put_checkbox("warning", options=["我已阅读以上提示并将合规使用文章内容"])
+    put_button("下载纯文本格式", onclick=lambda: download_content("txt"))
+    put_button("下载 Markdown 格式", onclick=lambda: download_content("markdown"))
+
 def index():
     put_markdown(f"""
     # 简书小工具集
@@ -146,5 +193,15 @@ def index():
     put_link("点击进入", url=f"http://{host}:{port}/?app=URLSchemeCoverter")
     # TODO: 不明原因导致直接跳转报错，暂时避开该问题，等待修复
     # put_button("点击进入", color="success", onclick=lambda:go_app(URLSchemeCoverter, new_window=False))
+    
+    put_markdown("""
+    ## 文章下载工具
+    下载简书中的文章内容，并将其保存至本地。
+    
+    服务状态：<font color=#008700>**正常运行**</font>
+    """, lstrip=True)
+    put_link("点击进入", url=f"http://{host}:{port}/?app=ArticleDownloader")
+    # TODO: 不明原因导致直接跳转报错，暂时避开该问题，等待修复
+    # put_button("点击进入", color="success", onclick=lambda:go_app(URLSchemeCoverter, new_window=False))
 
-start_server([index, UserAssetsViewer, URLSchemeCoverter], port=8602)
+start_server([index, UserAssetsViewer, URLSchemeCoverter, ArticleDownloader], port=8602)
