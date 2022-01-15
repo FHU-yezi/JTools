0ea031ab57088aa0c8231b62e90faae4c514ce32
FHU-yezi <yehaowei20060411@qq.com>
2022-01-15 13:58:38 +0800

    style：部分常量重命名和方法层级调整

--- modules/article_wordcloud_generator.py
+++ modules/article_wordcloud_generator.py
@@ -15,27 +15,28 @@ from .utils import LinkInHTML, SetFooter
 
 jieba.setLogLevel(jieba.logging.ERROR)  # 关闭 jieba 的日志输出
 
-stopwords = [word for word in open("wordcloud_assets/stopwords.txt", encoding="utf-8")]  # 预加载停用词词库
+STOPWORDS = [word for word in open("wordcloud_assets/stopwords.txt", encoding="utf-8")]  # 预加载停用词词库
 [jieba.add_word(word) for word in open("wordcloud_assets/hotwords.txt", encoding="utf-8")]  # 将热点词加入词库
+
+
+def GeneratorWordcloud():
+    try:
+        AssertArticleUrl(pin.pin["url"])
+    except InputError:
+        toast("输入的 URL 无效，请检查", color="error")
+        return  # 发生错误，不再运行后续逻辑
+
+    text = GetArticleText(pin.pin["url"])
+    cutted_text = jieba.cut(text)
+    cutted_text = [word for word in cutted_text if len(word) > 1 and word not in STOPWORDS]
+    wordcloud = WordCloud(font_path="font.otf", width=1280, height=720, background_color="white")
+    img = wordcloud.generate_from_frequencies(Counter(cutted_text))
+    img = Image.fromarray(img.to_array())
+    put_image(img)
 
 
 def ArticleWordcloudGenerator():
     """文章词云图生成工具"""
-
-    def GeneratorWordcloud():
-        try:
-            AssertArticleUrl(pin.pin["url"])
-        except InputError:
-            toast("输入的 URL 无效，请检查", color="error")
-            return  # 发生错误，不再运行后续逻辑
-
-        text = GetArticleText(pin.pin["url"])
-        cutted_text = jieba.cut(text)
-        cutted_text = [word for word in cutted_text if len(word) > 1 and word not in stopwords]
-        wordcloud = WordCloud(font_path="font.otf", width=1280, height=720, background_color="white")
-        img = wordcloud.generate_from_frequencies(Counter(cutted_text))
-        img = Image.fromarray(img.to_array())
-        put_image(img)
 
     put_markdown("""
     # 文章词云图生成工具
