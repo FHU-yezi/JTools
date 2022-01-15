037661a4f4071f16903261d5ace73d34d89194c3
FHU-yezi <yehaowei20060411@qq.com>
2022-01-15 22:38:35 +0800

    feat：更改服务跳转相关代码

--- main.py
+++ main.py
@@ -9,31 +9,30 @@ from modules.article_wordcloud_generator import ArticleWordcloudGenerator
 from modules.diszeroer_helper import DiszeroerHelper
 from modules.url_scheme_converter import URLSchemeConverter
 from modules.user_assets_viewer import UserAssetsViewer
-from modules.utils import SetFooter
+from modules.utils import SetFooter, GetUrl
 from modules.wordage_statistics_tool import WordageStatisticsTool
 
 __version__ = "0.5.0"
 
-DEBUG_MODE = True  # 调试模式
 
-if DEBUG_MODE:
-    host = "127.0.0.1"  # 本地地址
-    port = "8602"
-    print("调试模式已开启！")
-else:
-    host = "120.27.239.120"  # 服务器地址
-    port = "8602"
+STATUS_TO_TEXT = {-1: "暂停服务", 0: "正常运行", 1: "降级运行"}
 
-status_to_text = {-1: "暂停服务", 0: "正常运行", 1: "降级运行"}
+STATUS_TO_BUTTON_COLOR_TEXT = {-1: "danger", 0: "success", 1: "warning"}
 
-status_to_color = {-1: "#FF2D10", 0: "#008700", 1: "#FF8C00"}
+STATUS_TO_COLOR_HEX = {-1: "#FF2D10", 0: "#008700", 1: "#FF8C00"}
 
 
 def index():
+    """简书小工具集
+
+    为简友提供高效便捷的科技工具
+    """
     put_markdown(f"""
     # 简书小工具集
     为简友提供高效便捷的科技工具。
+
     Made with [JRT](https://github.com/FHU-yezi/JianshuResearchTools) and ♥
+    OpenSource On GitHub：[JianshuMicroFeatures](https://github.com/FHU-yezi/JianshuMicroFeatures)
     Version：{__version__}
     """)
 
@@ -42,7 +41,7 @@ def index():
     config.sort(key=lambda x: x["on_top"], reverse=True)  # 置顶的服务排在前面
 
     for service in config:
-        put_markdown(f"## {service['title']}")
+        put_markdown(f"## {service['name']}")
 
         if service["on_top"]:
             put_button("置顶", color="success", small=True, onclick=lambda: None)  # 显示置顶标签，点击无效果
@@ -56,13 +55,10 @@ def index():
         put_markdown(f"""
         {service["description"]}
 
-        服务状态：<font color={status_to_color[service["status"]]}>**{status_to_text[service["status"]]}**</font>
+        服务状态：<font color={STATUS_TO_COLOR_HEX[service["status"]]}>**{STATUS_TO_TEXT[service["status"]]}**</font>
         """)
-
         if service["status"] >= 0:  # 只有服务正常运行时才允许跳转
-            put_link("点击进入", url=f"http://{host}:{port}/?app={service['service_func_name']}")
-            # TODO: 不明原因导致直接跳转报错，暂时避开该问题，等待修复
-            # put_button("点击进入", color="success", onclick=lambda:go_app(app_name, new_window=False))
+            put_link("点击进入", url=f"{GetUrl()}?app={service['service_func_name']}")
 
     SetFooter(f"Version {__version__}, Made with ♥")
 
