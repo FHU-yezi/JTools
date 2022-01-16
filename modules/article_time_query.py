6638fc32d167f2704420a36c62794dbaae5adf8c
FHU-yezi <yehaowei20060411@qq.com>
2022-01-16 11:13:04 +0800

    feat：添加文章时间查询工具

--- modules/article_time_query.py
+++ modules/article_time_query.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+from JianshuResearchTools.article import (GetArticlePublishTime,
+                                          GetArticleTitle,
+                                          GetArticleUpdateTime)
 from JianshuResearchTools.exceptions import InputError, ResourceError
-from JianshuResearchTools.user import GetUserName, GetUserVIPInfo
 from pywebio.output import put_button, put_markdown, toast, use_scope
 from pywebio.pin import pin, put_input
 
@@ -31,8 +33,12 @@ def TimeDeltaFormat(td_object):
 def QueryUserVIPInfo():
     url = pin["url"]
     try:
-        user_name = GetUserName(url)
-        result = GetUserVIPInfo(url)
+        article_title = GetArticleTitle(url)
+        publish_time = GetArticlePublishTime(url).replace(tzinfo=None)
+        update_time = GetArticleUpdateTime(url)
+        is_updateed = "是" if publish_time != update_time else "否"
+        publish_timedelta = datetime.now() - publish_time.replace(tzinfo=None)
+        update_timedelta = datetime.now() - update_time.replace(tzinfo=None)
     except (InputError, ResourceError):
         toast("输入的 URL 无效，请检查", color="error")
         return  # 发生错误，不再运行后续逻辑
@@ -40,34 +46,28 @@ def QueryUserVIPInfo():
     with use_scope("output", clear=True):
         put_markdown("---")  # 分割线
 
-        if not result["vip_type"]:  # 没有 VIP
-            put_markdown(f"""
-            **查询结果**
-            作者名：{user_name}
-            VIP 等级：无 VIP
-            """)
-        else:
-            remaining_time = result["expire_date"] - datetime.now()
-            put_markdown(f"""
-            **查询结果**
-            作者名：{user_name}
-            VIP 等级：{result["vip_type"]}
-            VIP 到期时间：{result["expire_date"]}
-            VIP 剩余时间：{TimeDeltaFormat(remaining_time)}
-            """)
+        put_markdown(f"""
+        # 查询结果
+        文章标题：{article_title}
+        发布时间：{publish_time}
+        更新时间：{update_time}
+        更新过：{is_updateed}
+        文章在 {TimeDeltaFormat(publish_timedelta)} 之前发布
+        文章在 {TimeDeltaFormat(update_timedelta)} 之前最后一次更新
+        """)
 
 
-def UserVIPStatusQuery():
-    """会员状态查询工具"""
+def ArticleTimeQuery():
+    """文章时间查询工具"""
 
     put_markdown("""
-    # 会员状态查询工具
-    查询用户的会员状态。
+    # 文章时间查询工具
+    查询文章的发布时间和更新时间。
 
     数据来自简书官方接口。
     """)
 
-    put_input("url", label="请输入用户 URL：")
+    put_input("url", label="请输入文章 URL：")
     put_button("查询", onclick=QueryUserVIPInfo)
 
     SetFooter(f"Powered By \
