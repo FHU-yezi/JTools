e1f3c31858f8b09c7edb9c92921d9ca86c42ea81
yezi <yehaowei20060411@qq.com>
2022-08-30 23:13:24 +0800

    feat: 配置文件模块支持使用点标记法取值

--- config_manager.py
+++ config_manager.py
@@ -1,4 +1,5 @@
 from os import path as os_path
+from typing import Any, Dict
 
 from yaml import SafeLoader
 from yaml import dump as yaml_dump
@@ -38,21 +39,30 @@ class Config():
         if not os_path.exists("config.yaml"):  # 没有配置文件
             with open("config.yaml", "w", encoding="utf-8") as f:
                 yaml_dump(_DEFAULT_CONFIG, f, allow_unicode=True, indent=4)
-            self._config_dict = _DEFAULT_CONFIG
+            self._data = _DEFAULT_CONFIG
         else:  # 有配置文件
             with open("config.yaml", "r", encoding="utf-8") as f:
-                self._config_dict = yaml_load(f, Loader=SafeLoader)
+                self._data = yaml_load(f, Loader=SafeLoader)
 
-    def __getitem__(self, item):
-        self.refresh()  # 刷新配置文件以应用更改
-        item_path = item.split("/")
-        result = self._config_dict
-        for now_path in item_path:
-            result = result[now_path]
-        return result
+    def __getattr__(self, name: str) -> Any:
+        self.refresh()
+
+        result: Any = self._data[name]
+        if isinstance(result, dict):
+            return ConfigNode(result)
+        else:
+            return result
 
     def refresh(self):
         self.__init__()
+
+
+class ConfigNode():
+    def __init__(self, data: Dict[str, Any]) -> None:
+        self._data: Dict[str, Any] = data
+
+    def __getattr__(self, name: str) -> Any:
+        return self._data[name]
 
 
 def InitConfig() -> Config:
