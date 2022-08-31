8d2ec5c626246c68d5b9f95c9cbfcc4e2d8b6401
yezi <yehaowei20060411@qq.com>
2022-08-31 21:12:27 +0800

    feat: 新增上榜文章查询模块

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -3,13 +3,15 @@ from pymongo import MongoClient
 from utils.config_manager import config
 
 
-def init_DB():
+def init_DB(db_name: str):
     connection: MongoClient = MongoClient(config.db.host, config.db.port)
-    db = connection[config.db.main_database]
+    db = connection[db_name]
     return db
 
 
-db = init_DB()
+db = init_DB(config.db.main_database)
 
 run_log_db = db.run_log
 access_log_db = db.access_log
+
+article_FP_rank_db = init_DB("JFetcherData").article_FP_rank
