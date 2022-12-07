cbbf55b98df64ce6353c5864e14c27c41c74092a
yezi <yehaowei20060411@qq.com>
2022-12-07 11:57:30 +0800

    feat: LP 理事会推文检测工具页面优化

--- app/organization/LP_recommend_checker.py
+++ app/organization/LP_recommend_checker.py
@@ -8,11 +8,11 @@ from JianshuResearchTools.exceptions import InputError, ResourceError
 from JianshuResearchTools.objects import Article
 from pywebio.output import put_button, put_collapse, put_markdown, use_scope
 from pywebio.pin import pin, put_input
-from utils.cache import timeout_cache
 
+from utils.cache import timeout_cache
 from utils.callback import bind_enter_key_callback
 from utils.db import article_FP_rank_db
-from utils.html import green_text, link, red_text
+from utils.html import green_text, grey_text, link, red_text
 from utils.text_filter import input_filter
 from utils.time_helper import human_readable_td_to_now
 from utils.widgets import (
@@ -98,9 +98,9 @@ def on_rank_last_1m_top30_checker(article_obj: Article) -> Tuple[bool, int, int]
 CHECK_ITEM_FUNC_MAPPING: Dict[str, Callable] = {
     "文章字数": wordage_checker,
     "文章收益": reward_checker,
-    "过去 7 天上榜次数": on_rank_last_7d_checker,
-    "过去 10 天前 30 名次数": on_rank_last_10d_top30_checker,
-    "过去 1 个月前 30 名次数": on_rank_last_1m_top30_checker,
+    "作者过去 7 天上榜次数": on_rank_last_7d_checker,
+    "作者过去 10 天前 30 名次数": on_rank_last_10d_top30_checker,
+    "作者过去 1 个月前 30 名次数": on_rank_last_1m_top30_checker,
 }
 
 
@@ -139,9 +139,11 @@ def on_check_button_clicked() -> None:
                 if not passed:
                     failed_items.append(item_name)
 
-                put_collapse(
-                    title=("通过" if passed else "不通过") + " | " + item_name,
-                    content=put_markdown(f"限制：{limit}\n实际：{actual}"),
+                put_markdown(
+                    (green_text("通过") if passed else red_text("不通过"))
+                    + " | "
+                    + item_name
+                    + grey_text(f"（限制：{limit}，实际：{actual}）")
                 )
 
     # 由于 result scope 中已有内容，这里不能使用 use_result_scope
@@ -153,7 +155,8 @@ def on_check_button_clicked() -> None:
             # 构建 Markdown 格式的列表
             failed_items_text: str = "- " + "\n- ".join(failed_items)
             put_markdown(red_text("**该文章不符合推文要求**"))
-            put_markdown(f"以下检查未通过：\n{failed_items_text}")
+            put_markdown(f"未通过条件：\n{failed_items_text}")
+            put_markdown("---")
 
 
 def LP_recommend_checker() -> None:
