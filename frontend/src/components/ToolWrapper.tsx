d30d50b98acecde3dcccd15968fbbd5eb5633abf
yezi <yehaowei20060411@qq.com>
2023-06-03 21:34:16 +0800

    fix: 修复顶栏被部分元素遮盖的问题

--- frontend/src/components/ToolWrapper.tsx
+++ frontend/src/components/ToolWrapper.tsx
@@ -18,6 +18,7 @@ export default function ToolWrapper({ Component, toolName }: Props) {
           height: "4em",
           width: "100%",
           display: "flex",
+          zIndex: 1,
         }}
       >
         <Header toolName={toolName} showBackArrow={true} />
