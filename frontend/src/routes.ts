571e3428c7279891fdb4e5ca5eca1d23f64cc548
yezi <yehaowei20060411@qq.com>
2023-06-11 15:11:38 +0800

    feat: 工具更名

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -13,19 +13,19 @@ export const routes: RouteItem[] = [
   {
     component: lazy(() => import("./pages/LPRecommendChecker")),
     path: "/LP-recommend-checker",
-    toolName: "LP 理事会推文检测工具",
+    toolName: "LP 理事会推文检测",
     description: "检测文章是否符合 LP 理事会推荐标准。",
   },
   {
     component: lazy(() => import("./pages/OnRankArticleViewer")),
     path: "/on-rank-article-viewer",
-    toolName: "上榜文章查询工具",
+    toolName: "上榜文章查询",
     description: "查询文章上榜历史。",
   },
   {
     component: lazy(() => import("./pages/JPEPFTNMacketAnalyzer")),
     path: "/JPEP-FTN-market-analyzer",
-    toolName: "积分兑换平台贝市分析工具",
+    toolName: "商城贝市场分析",
     description: "分析简书积分兑换平台贝市数据。",
   },
   {
@@ -37,31 +37,31 @@ export const routes: RouteItem[] = [
   {
     component: lazy(() => import("./pages/LotteryAnalyzer")),
     path: "/lottery-analyzer",
-    toolName: "抽奖分析工具",
+    toolName: "简书大转盘分析",
     description: "分析简书大转盘中奖数据。",
   },
   {
     component: lazy(() => import("./pages/ArticlePublishTimeViewer")),
     path: "/article-publish-time-viewer",
-    toolName: "文章发布时间查询工具",
+    toolName: "文章发布时间查询",
     description: "查询文章的发布与更新时间。",
   },
   {
     component: lazy(() => import("./pages/ArticleWordcloudGenerator")),
     path: "/article-wordcloud-generator",
-    toolName: "文章词云图生成工具",
+    toolName: "文章词云图",
     description: "生成文章词云图。",
   },
   {
     component: lazy(() => import("./pages/URLSchemeConvertor")),
     path: "/URL-scheme-convertor",
-    toolName: "URL Scheme 转换工具",
+    toolName: "URL Scheme 转换",
     description: "将简书网页链接转换成 URL Scheme，以实现跳转 App 打开。",
   },
   {
     component: lazy(() => import("./pages/VIPInfoViewer")),
     path: "/VIP-info-viewer",
-    toolName: "会员信息查询工具",
+    toolName: "会员信息查询",
     description: "查询简书会员等级和过期时间。",
   },
 ];
