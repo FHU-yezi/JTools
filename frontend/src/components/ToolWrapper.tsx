33e9355ec419fd5bd600a3bdf3a96a15c8a6d14e
yezi <yehaowei20060411@qq.com>
2023-06-17 08:31:45 +0800

    feat: 工具页面展示降级和不可用提示

--- frontend/src/components/ToolWrapper.tsx
+++ frontend/src/components/ToolWrapper.tsx
@@ -1,6 +1,11 @@
-import { Flex } from "@mantine/core";
+import {
+  Button, Flex, Modal, Stack,
+} from "@mantine/core";
+import { notifications } from "@mantine/notifications";
 import { batch, useSignal } from "@preact/signals";
 import { JSX, Suspense, useEffect } from "preact/compat";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import { useLocation } from "wouter-preact";
 import { InfoRequest, InfoResponse, InfoStatus } from "../models/info";
 import { getToolSlug } from "../utils/URLHelper";
 import { commonAPIErrorHandler } from "../utils/errorHandler";
@@ -16,6 +21,8 @@ interface Props {
 }
 
 export default function ToolWrapper({ Component, toolName }: Props) {
+  const [, setLocation] = useLocation();
+
   const isLoading = useSignal(false);
   const hasResult = useSignal(false);
   const toolStatus = useSignal(InfoStatus.NORMAL);
@@ -24,6 +31,7 @@ export default function ToolWrapper({ Component, toolName }: Props) {
   const dataUpdateTime = useSignal<Date | undefined>(undefined);
   const dataUpdateFreqDesc = useSignal<string | undefined>(undefined);
   const dataCount = useSignal<number | undefined>(undefined);
+  const showUnavaliableModal = useSignal(false);
 
   // 处理部分情况下页面切换后不在顶部的问题
   useEffect(() => window.scrollTo(0, 0));
@@ -49,6 +57,20 @@ export default function ToolWrapper({ Component, toolName }: Props) {
               dataCount.value = data.data_count;
             }
           });
+          if (toolStatus.value === InfoStatus.DOWNGRADED) {
+            notifications.show({
+              title: "服务降级",
+              message: downgradedReason.value.length !== 0
+                ? downgradedReason.value
+                : "该小工具处于降级状态，其数据准确性、展示效果及性能可能受到影响，请您留意。",
+              color: "orange",
+              autoClose: false,
+              icon: <AiOutlineArrowDown size="1.2em" color="white" />,
+            });
+          }
+          if (toolStatus.value === InfoStatus.UNAVALIABLE) {
+            showUnavaliableModal.value = true;
+          }
         },
         commonAPIErrorHandler,
         hasResult,
@@ -95,6 +117,26 @@ export default function ToolWrapper({ Component, toolName }: Props) {
           </>
         ) : <Loading />}
       </Suspense>
+      <Modal
+        opened={showUnavaliableModal.value}
+        onClose={() => null}
+        title="服务不可用"
+        closeOnClickOutside={false}
+        closeOnEscape={false}
+        withCloseButton={false}
+      >
+        <Stack>
+          {unavaliableReason.value.length !== 0
+            ? unavaliableReason.value
+            : "该小工具由于数据准确性、体验或安全性等原因暂时不可用，请稍后再尝试访问，并留意相关公告。"}
+          <Button
+            variant="light"
+            onClick={() => setLocation("/")}
+          >
+            返回首页
+          </Button>
+        </Stack>
+      </Modal>
     </>
   );
 }
