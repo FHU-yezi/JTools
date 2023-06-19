34906cb1fb81d0091fc6717934a7adea1538b5ee
yezi <yehaowei20060411@qq.com>
2023-06-19 16:15:52 +0800

    fix: 修复 v2 链接跳转异常 umami is undefined 的问题

--- frontend/src/utils/umamiTrack.ts
+++ frontend/src/utils/umamiTrack.ts
@@ -2,6 +2,11 @@ export default function umamiTrack(
   eventName?: string,
   eventData?: { [key: string]: string | number },
 ) {
-  // eslint-disable-next-line no-undef
-  umami.track(eventName, eventData);
+  try {
+    // eslint-disable-next-line no-undef
+    umami.track(eventName, eventData);
+  } catch {
+    // eslint-disable-next-line no-console
+    console.error(`上报事件失败：${eventName}`);
+  }
 }
