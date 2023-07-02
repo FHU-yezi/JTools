83a46769f22ce0fb45db4f9273fbfa98dff7c22f
yezi <yehaowei20060411@qq.com>
2023-07-02 09:11:26 +0800

    feat: 优化首页卡片展示效果

--- frontend/src/components/ToolCard.tsx
+++ frontend/src/components/ToolCard.tsx
@@ -22,28 +22,25 @@ export default function ToolCard({
   return (
     <button
       type="button"
-      className="flex w-full items-center justify-between rounded-2xl border bg-white p-5 shadow dark:border-gray-700 dark:bg-gray-900"
+      className="flex w-full items-center justify-between rounded-2xl border bg-white p-5 shadow transition-colors hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:hover:bg-gray-800"
       onClick={!unavaliable ? () => setLocation(path) : undefined}
     >
-      <div>
-        <div>
-          <div className="flex gap-2">
-            <SSText className="font-bold" large>
-              {toolName}
-            </SSText>
-            {downgraded && (
-              <SSBadge className="bg-orange-200 text-orange-500 dark:bg-orange-950">
-                降级
-              </SSBadge>
-            )}
-            {unavaliable && (
-              <SSBadge className="bg-red-200 text-red-500 dark:bg-red-950">
-                不可用
-              </SSBadge>
-            )}
-          </div>
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-2">
+          <SSText className="font-bold" large>
+            {toolName}
+          </SSText>
+          {downgraded && (
+            <SSBadge className="bg-orange-200 text-orange-500 dark:bg-orange-950">
+              降级
+            </SSBadge>
+          )}
+          {unavaliable && (
+            <SSBadge className="bg-red-200 text-red-500 dark:bg-red-950">
+              不可用
+            </SSBadge>
+          )}
         </div>
-        <div className="h-4" />
         <SSText className="text-left">{description}</SSText>
       </div>
       {!unavaliable && (
