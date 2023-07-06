e936033374b81ebe56c01d8214a0f594e6a5f752
yezi <yehaowei20060411@qq.com>
2023-07-06 16:05:28 +0800

    feat: 优化小工具页面加载逻辑

--- frontend/src/components/ToolWrapper.tsx
+++ frontend/src/components/ToolWrapper.tsx
@@ -28,10 +28,9 @@ export default function ToolWrapper({ Component, toolName }: Props) {
   const [, setLocation] = useLocation();
 
   const isLoading = useSignal(false);
-  const hasResult = useSignal(false);
-  const toolStatus = useSignal(InfoStatus.NORMAL);
-  const unavaliableReason = useSignal("");
-  const downgradedReason = useSignal("");
+  const toolStatus = useSignal<InfoStatus | undefined>(undefined);
+  const unavaliableReason = useSignal<string | undefined>(undefined);
+  const downgradedReason = useSignal<string | undefined>(undefined);
   const dataUpdateTime = useSignal<Date | undefined>(undefined);
   const dataUpdateFreqDesc = useSignal<string | undefined>(undefined);
   const dataCount = useSignal<number | undefined>(undefined);
@@ -66,24 +65,25 @@ export default function ToolWrapper({ Component, toolName }: Props) {
               dataCount.value = data.data_count;
             }
           });
+
           if (toolStatus.value === InfoStatus.DOWNGRADED) {
             notifications.show({
               title: "服务降级",
               message:
-                downgradedReason.value.length !== 0
-                  ? downgradedReason.value
-                  : "该小工具处于降级状态，其数据准确性、展示效果及性能可能受到影响，请您留意。",
+                downgradedReason.value ??
+                "该小工具处于降级状态，其数据准确性、展示效果及性能可能受到影响，请您留意。",
               color: "orange",
               autoClose: false,
               icon: <AiOutlineArrowDown size="1.2em" color="white" />,
             });
           }
+
           if (toolStatus.value === InfoStatus.UNAVALIABLE) {
             showUnavaliableModal.value = true;
           }
         },
         commonAPIErrorHandler,
-        hasResult,
+        undefined,
         isLoading
       );
     } catch {}
@@ -92,50 +92,52 @@ export default function ToolWrapper({ Component, toolName }: Props) {
   return (
     <>
       <Header toolName={toolName} showBackArrow />
-      <Suspense fallback={<Loading />}>
-        {hasResult.value ? (
-          <>
-            <div
-              className={clsx("flex gap-6", {
-                "my-4":
-                  typeof dataUpdateTime.value !== "undefined" ||
-                  typeof dataCount.value !== "undefined",
-              })}
-            >
-              {typeof dataUpdateTime.value !== "undefined" && (
-                <SSStat
-                  className="flex-grow"
-                  title="数据更新时间"
-                  value={getDateTimeWithoutSecond(dataUpdateTime.value!)}
-                  desc={dataUpdateFreqDesc.value}
-                />
-              )}
-              {typeof dataCount.value !== "undefined" && (
-                <SSStat
-                  className="flex-grow"
-                  title="总数据量"
-                  value={dataCount.value}
-                />
-              )}
-            </div>
-            {typeof dataSource.value !== "undefined" && (
-              <div className="my-4 flex flex-col gap-1">
-                <SSText bold>数据来源</SSText>
-                {Object.entries(dataSource.value).map(([name, url]) => (
-                  <SSText>
-                    {name}
-                    ：
-                    <SSLink url={url} isExternal />
-                  </SSText>
-                ))}
-              </div>
+      {!isLoading.value ? (
+        <>
+          <div
+            className={clsx("flex gap-6", {
+              "my-4":
+                typeof dataUpdateTime.value !== "undefined" &&
+                typeof dataCount.value !== "undefined",
+            })}
+          >
+            {typeof dataUpdateTime.value !== "undefined" && (
+              <SSStat
+                className="flex-grow"
+                title="数据更新时间"
+                value={getDateTimeWithoutSecond(dataUpdateTime.value!)}
+                desc={dataUpdateFreqDesc.value}
+              />
             )}
-            <Component />
-          </>
-        ) : (
-          <Loading />
-        )}
-      </Suspense>
+            {typeof dataCount.value !== "undefined" && (
+              <SSStat
+                className="flex-grow"
+                title="总数据量"
+                value={dataCount.value}
+              />
+            )}
+          </div>
+          {typeof dataSource.value !== "undefined" && (
+            <div className="my-4 flex flex-col gap-1">
+              <SSText bold>数据来源</SSText>
+              {Object.entries(dataSource.value).map(([name, url]) => (
+                <SSText>
+                  {name}
+                  ：
+                  <SSLink url={url} isExternal />
+                </SSText>
+              ))}
+            </div>
+          )}
+
+          <Suspense fallback={<Loading />}>
+            {!isLoading.value && <Component />}
+          </Suspense>
+        </>
+      ) : (
+        <Loading />
+      )}
+
       <Modal
         opened={showUnavaliableModal.value}
         onClose={() => null}
@@ -145,9 +147,8 @@ export default function ToolWrapper({ Component, toolName }: Props) {
         withCloseButton={false}
       >
         <div className="flex flex-col gap-4">
-          {unavaliableReason.value.length !== 0
-            ? unavaliableReason.value
-            : "该小工具由于数据准确性、体验或安全性等原因暂时不可用，请稍后再尝试访问，并留意相关公告。"}
+          {unavaliableReason.value ??
+            "该小工具由于数据准确性、体验或安全性等原因暂时不可用，请稍后再尝试访问，并留意相关公告。"}
           <SSButton onClick={() => setLocation("/")}>返回首页</SSButton>
         </div>
       </Modal>
