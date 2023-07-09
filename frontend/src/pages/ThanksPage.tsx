ca1428f24d995f89b2f00560a9c7d77ba4a993aa
yezi <yehaowei20060411@qq.com>
2023-07-09 08:39:53 +0800

    feat: 优化鸣谢页面呈现效果

--- frontend/src/pages/ThanksPage.tsx
+++ frontend/src/pages/ThanksPage.tsx
@@ -36,39 +36,49 @@ export default function ThanksPage() {
         <SSText xlarge xbold>
           开源库
         </SSText>
-        {Object.entries(opensourcePackages).map(([name, url]) => (
-          <SSText>
-            {name}
-            ：
-            <SSLink
-              url={url}
-              label={`${url.split("/")[4]} - GitHub`}
-              isExternal
-            />
-          </SSText>
-        ))}
+        <div className="columns-1 gap-4 space-y-4 sm:columns-2">
+          {Object.entries(opensourcePackages).map(([partName, part]) => (
+            <div className="flex flex-col gap-4 rounded-2xl border bg-white p-4 shadow dark:bg-gray-900">
+              <SSText bold large>
+                {partName}
+              </SSText>
+              {part.map(({ name, desc, url }) => (
+                <SSText>
+                  {desc}：
+                  <SSLink label={name} url={url} isExternal />
+                </SSText>
+              ))}
+            </div>
+          ))}
+        </div>
 
         <SSText xlarge xbold>
           「捉虫计划」反馈
         </SSText>
-        {debugProjectRecords.map((item) => (
-          <div className="rounded-2xl border p-4 shadow">
-            <div className="flex flex-col gap-3">
-              <div className="flex flex-col gap-0.5">
-                <SSText bold large>{`${item.time} | ${item.type}`}</SSText>
-                <SSText small gray>
-                  {item.module}
+        <div className="row-auto grid grid-cols-1 gap-4 sm:grid-cols-2">
+          {debugProjectRecords.map((item) => (
+            <div className="rounded-2xl border p-4 shadow">
+              <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-0.5">
+                  <SSText bold large>{`${item.time} | ${item.type}`}</SSText>
+                  <SSText small gray>
+                    {item.module}
+                  </SSText>
+                </div>
+                <SSText>{item.desc}</SSText>
+                <SSText>
+                  反馈者：
+                  <SSLink
+                    url={item.user_url}
+                    label={item.user_name}
+                    isExternal
+                  />
                 </SSText>
+                <SSText>{`奖励：${item.award} 简书贝`}</SSText>
               </div>
-              <SSText>{item.desc}</SSText>
-              <SSText>
-                反馈者：
-                <SSLink url={item.user_url} label={item.user_name} isExternal />
-              </SSText>
-              <SSText>{`奖励：${item.award} 简书贝`}</SSText>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
 
         <div className="h-9" />
         <SSText large center>
