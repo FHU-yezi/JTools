3b542107dc5d8f6008d7476b9f212ae91e3b9eb4
yezi <yehaowei20060411@qq.com>
2023-07-21 11:02:50 +0800

    feat: 优化中奖记录查询工具展示

--- frontend/src/pages/LotteryRewardRecordViewer.tsx
+++ frontend/src/pages/LotteryRewardRecordViewer.tsx
@@ -1,9 +1,9 @@
-import { Chip } from "@mantine/core";
-import { batch, signal } from "@preact/signals";
+import { Signal, batch, effect, signal, useSignal } from "@preact/signals";
 import { DataTable } from "mantine-datatable";
 import { useEffect } from "preact/hooks";
 import toast from "react-hot-toast";
 import SSButton from "../components/SSButton";
+import SSCheckbox from "../components/SSCheckbox";
 import SSSkeleton from "../components/SSSkeleton";
 import SSText from "../components/SSText";
 import SSTextInput from "../components/SSTextInput";
@@ -56,6 +56,57 @@ function handleQuery(offset: number) {
   } catch {}
 }
 
+function RewardsFliter() {
+  const rewardSelectedSignals = useSignal<Record<string, Signal<boolean>>>({});
+  const dataReady = useSignal(false);
+
+  useEffect(() => {
+    try {
+      fetchData<Record<string, never>, RewardResponse>(
+        "GET",
+        "/tools/lottery_reward_record_viewer/rewards",
+        {},
+        (data) =>
+          batch(() => {
+            rewards.value = data.rewards;
+            selectedRewards.value = data.rewards.map((item) =>
+              replaceAll(item, " ", "")
+            );
+            rewards.value.forEach(
+              (name) => (rewardSelectedSignals.value[name] = signal(false))
+            );
+            dataReady.value = true;
+          }),
+        commonAPIErrorHandler
+      );
+    } catch {}
+  }, []);
+
+  effect(
+    () =>
+      (selectedRewards.value = Object.keys(rewardSelectedSignals.value).filter(
+        (name) => rewardSelectedSignals.value[name].value === true
+      ))
+  );
+
+  return (
+    <div>
+      <SSText className="mb-1.5" bold>
+        奖项筛选
+      </SSText>
+      {dataReady.value ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          {Object.entries(rewardSelectedSignals.value).map(([name, value]) => (
+            <SSCheckbox label={name} value={value} />
+          ))}
+        </div>
+      ) : (
+        <SSSkeleton className="h-9 w-full" />
+      )}
+    </div>
+  );
+}
+
 function ResultTable() {
   return (
     <DataTable
@@ -86,24 +137,6 @@ function ResultTable() {
 }
 
 export default function LotteryRewardRecordViewer() {
-  useEffect(() => {
-    try {
-      fetchData<Record<string, never>, RewardResponse>(
-        "GET",
-        "/tools/lottery_reward_record_viewer/rewards",
-        {},
-        (data) =>
-          batch(() => {
-            rewards.value = data.rewards;
-            selectedRewards.value = data.rewards.map((item) =>
-              replaceAll(item, " ", "")
-            );
-          }),
-        commonAPIErrorHandler
-      );
-    } catch {}
-  }, []);
-
   return (
     <div className="flex flex-col gap-4">
       <SSTextInput
@@ -111,29 +144,7 @@ export default function LotteryRewardRecordViewer() {
         value={userURL}
         onEnter={() => handleQuery(0)}
       />
-      {typeof rewards.value !== "undefined" ? (
-        <>
-          <SSText bold>奖项筛选</SSText>
-          <Chip.Group
-            value={selectedRewards.value}
-            onChange={(value) => (selectedRewards.value = value)}
-            multiple
-          >
-            <div className="flex flex-wrap gap-3">
-              {rewards.value.map((item) => (
-                <Chip
-                  key={replaceAll(item, " ", "")}
-                  value={replaceAll(item, " ", "")}
-                >
-                  {item}
-                </Chip>
-              ))}
-            </div>
-          </Chip.Group>
-        </>
-      ) : (
-        <SSSkeleton className="h-16" />
-      )}
+      <RewardsFliter />
       <SSTooltip tooltip="受简书接口限制，我们无法获取这两种奖品的中奖情况，故无法进行查询">
         关于免费开 1 次连载 / 锦鲤头像框
       </SSTooltip>
