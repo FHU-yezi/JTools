2d2a7753c4f67c908fcb110ad902f28c899238dd
yezi <yehaowei20060411@qq.com>
2023-07-25 09:43:11 +0800

    feat: 优化 Checkbox 交互体验

--- frontend/src/components/SSCheckbox.tsx
+++ frontend/src/components/SSCheckbox.tsx
@@ -10,27 +10,32 @@ interface Props {
 
 export default function SSCheckbox({ label, value }: Props) {
   return (
-    <SSText className="flex select-none items-center gap-2">
-      <div
-        role="checkbox"
-        tabIndex={0}
-        aria-checked={value.value}
-        className={clsx(
-          "inline-flex h-5 w-5 items-center justify-center rounded transition-colors duration-100",
-          {
-            "gray-border color-layer-1 hover:bg-gray-100 dark:hover:bg-gray-700":
-              !value.value,
-            "bg-gray-800 hover:bg-gray-700": value.value,
-          }
-        )}
-        onClick={() => (value.value = !value.value)}
-        onKeyPress={() => (value.value = !value.value)}
-      >
-        {value.value && (
-          <BiCheck className="stroke-gray-100 stroke-2 dark:stroke-gray-300" />
-        )}
-      </div>
-      {label}
-    </SSText>
+    <button
+      type="button"
+      role="checkbox"
+      tabIndex={0}
+      className="w-fit"
+      aria-checked={value.value}
+      onClick={() => (value.value = !value.value)}
+      onKeyPress={() => (value.value = !value.value)}
+    >
+      <SSText className="flex select-none items-center gap-2">
+        <div
+          className={clsx(
+            "grid h-5 w-5 place-content-center rounded transition-colors duration-100",
+            {
+              "gray-border color-layer-1 hover:bg-gray-100 dark:hover:bg-gray-700":
+                !value.value,
+              "bg-gray-800 hover:bg-gray-700": value.value,
+            }
+          )}
+        >
+          {value.value && (
+            <BiCheck className="stroke-gray-100 stroke-2 dark:stroke-gray-300" />
+          )}
+        </div>
+        {label}
+      </SSText>
+    </button>
   );
 }
