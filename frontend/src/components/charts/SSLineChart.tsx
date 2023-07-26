78bf6768768ada4cdbeb895f0a4d10a5f29e0fb9
yezi <yehaowei20060411@qq.com>
2023-07-26 09:39:01 +0800

    feat: 引入 ECharts，封装常用图表类型

---
+++ frontend/src/components/charts/SSLineChart.tsx
@@ -0,0 +1,98 @@
+import { useLocalStorage } from "@mantine/hooks";
+import { useSignal } from "@preact/signals";
+import type { LineSeriesOption } from "echarts/charts";
+import { LineChart } from "echarts/charts";
+import type {
+  GridComponentOption,
+  LegendComponentOption,
+  TooltipComponentOption,
+} from "echarts/components";
+import {
+  GridComponent,
+  LegendComponent,
+  TooltipComponent,
+} from "echarts/components";
+import type { ComposeOption } from "echarts/core";
+import * as echarts from "echarts/core";
+import { SVGRenderer } from "echarts/renderers";
+import { useEffect, useRef } from "preact/hooks";
+
+echarts.use([
+  LineChart,
+  GridComponent,
+  LegendComponent,
+  TooltipComponent,
+  SVGRenderer,
+]);
+
+type OptionType = ComposeOption<
+  | LineSeriesOption
+  | GridComponentOption
+  | LegendComponentOption
+  | TooltipComponentOption
+>;
+
+interface Props {
+  className: string;
+  options: OptionType;
+  dataReady?: boolean;
+}
+
+export default function SSLineChart({
+  className,
+  options,
+  dataReady = true,
+}: Props) {
+  const [colorScheme] = useLocalStorage<"light" | "dark">({
+    key: "jtools-color-scheme",
+    defaultValue: "light",
+  });
+  const echartObject = useSignal<echarts.ECharts | undefined>(undefined);
+
+  const chartDivRef = useRef<HTMLDivElement>(null);
+
+  const observer = new ResizeObserver(() => echartObject.value!.resize());
+
+  useEffect(() => {
+    if (echartObject.value === undefined && chartDivRef.current !== null) {
+      echartObject.value = echarts.init(chartDivRef.current);
+      // 透明背景
+      echartObject.value.setOption({ ...options, backgroundColor: "" });
+      observer.observe(chartDivRef.current);
+
+      return () => {
+        echartObject.value!.dispose();
+        observer.unobserve(chartDivRef.current!);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    if (echartObject.value !== undefined && chartDivRef.current !== null) {
+      echartObject.value.dispose();
+      echartObject.value = echarts.init(
+        chartDivRef.current,
+        colorScheme === "dark" ? "dark" : undefined
+      );
+      // 透明背景
+      echartObject.value.setOption({ ...options, backgroundColor: "" });
+    }
+  }, [colorScheme]);
+
+  useEffect(() => {
+    if (echartObject.value !== undefined) {
+      if (!dataReady) {
+        echartObject.value.showLoading({
+          text: "   加载中...",
+          fontSize: 16,
+          spinnerRadius: 15,
+          lineWidth: 2,
+        });
+      } else {
+        echartObject.value.hideLoading();
+      }
+    }
+  }, [dataReady]);
+
+  return <div className={className} ref={chartDivRef} />;
+}
---
+++ frontend/src/components/charts/SSPieChart.tsx
@@ -0,0 +1,84 @@
+import { useLocalStorage } from "@mantine/hooks";
+import { useSignal } from "@preact/signals";
+import type { PieSeriesOption } from "echarts/charts";
+import { PieChart } from "echarts/charts";
+import type {
+  LegendComponentOption,
+  TooltipComponentOption,
+} from "echarts/components";
+import { LegendComponent, TooltipComponent } from "echarts/components";
+import type { ComposeOption } from "echarts/core";
+import * as echarts from "echarts/core";
+import { SVGRenderer } from "echarts/renderers";
+import { useEffect, useRef } from "preact/hooks";
+
+echarts.use([PieChart, LegendComponent, TooltipComponent, SVGRenderer]);
+
+type OptionType = ComposeOption<
+  PieSeriesOption | LegendComponentOption | TooltipComponentOption
+>;
+
+interface Props {
+  className: string;
+  options: OptionType;
+  dataReady?: boolean;
+}
+
+export default function SSPieChart({
+  className,
+  options,
+  dataReady = true,
+}: Props) {
+  const [colorScheme] = useLocalStorage<"light" | "dark">({
+    key: "jtools-color-scheme",
+    defaultValue: "light",
+  });
+  const echartObject = useSignal<echarts.ECharts | undefined>(undefined);
+
+  const chartDivRef = useRef<HTMLDivElement>(null);
+
+  const observer = new ResizeObserver(() => echartObject.value!.resize());
+
+  useEffect(() => {
+    if (echartObject.value === undefined && chartDivRef.current !== null) {
+      echartObject.value = echarts.init(chartDivRef.current);
+      // 透明背景
+      echartObject.value.setOption({ ...options, backgroundColor: "" });
+      observer.observe(chartDivRef.current);
+
+      return () => {
+        echartObject.value!.dispose();
+        observer.unobserve(chartDivRef.current!);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    if (echartObject.value !== undefined && chartDivRef.current !== null) {
+      echartObject.value.dispose();
+      echartObject.value = echarts.init(
+        chartDivRef.current,
+        colorScheme === "dark" ? "dark" : undefined
+      );
+      // 透明背景
+      echartObject.value.setOption({ ...options, backgroundColor: "" });
+    }
+  }, [colorScheme]);
+
+  useEffect(() => {
+    if (echartObject.value !== undefined) {
+      if (!dataReady) {
+        echartObject.value.showLoading({
+          text: "   加载中...",
+          fontSize: 16,
+          spinnerRadius: 15,
+          lineWidth: 2,
+        });
+      } else {
+        echartObject.value.hideLoading();
+      }
+    }
+  }, [dataReady]);
+
+  return <div className={className} ref={chartDivRef} />;
+}
