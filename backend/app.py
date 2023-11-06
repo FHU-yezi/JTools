28efcc31966354a8d5567baac27554a3e8d2e42e
yezi <yehaowei20060411@qq.com>
2023-11-06 17:39:50 +0800

    feat: 调整接口文档配置

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from litestar import Litestar
 from litestar.openapi import OpenAPIConfig
+from litestar.openapi.spec import Server
 from sspeedup.api.litestar import EXCEPTION_HANDLERS
 
 from api import API_ROUTER
@@ -10,5 +11,9 @@ app = Litestar(
     openapi_config=OpenAPIConfig(
         title="JTools API",
         version="v1.0.0",
+        servers=[Server(description="主端点", url="/")],
+        use_handler_docstrings=True,
+        root_schema_site="elements",
+        enabled_endpoints={"elements", "openapi.json"},
     ),
 )
