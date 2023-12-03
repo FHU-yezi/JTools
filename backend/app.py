58b0690c273a9219dc6f41b5230cc127ffa0d3cd
yezi <yehaowei20060411@qq.com>
2023-12-03 08:23:16 +0800

    fix: 修复无法直接从 API 文档发起请求的问题

--- backend/app.py
+++ backend/app.py
@@ -8,14 +8,14 @@ from api import API_ROUTER
 
 class CustomOpenAPIController(OpenAPIController):
     path = "/docs"
-    swagger_ui_version = "5.9.1"
+    swagger_ui_version = "5.10.3"
     swagger_css_url = (
-        "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.1/swagger-ui.css"
+        "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.10.3/swagger-ui.css"
     )
     swagger_ui_bundle_js_url = (
-        "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.1/swagger-ui-bundle.js"
+        "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.10.3/swagger-ui-bundle.js"
     )
-    swagger_ui_standalone_preset_js_url = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.1/swagger-ui-standalone-preset.js"
+    swagger_ui_standalone_preset_js_url = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.10.3/swagger-ui-standalone-preset.js"
     favicon_url = "https://tools.sscreator.com/favicon-vector.svg"
 
 
@@ -26,7 +26,7 @@ app = Litestar(
         openapi_controller=CustomOpenAPIController,
         title="JTools API",
         version="v1.0.0",
-        servers=[Server(description="主端点", url="/")],
+        servers=[Server(description="主端点", url="/api")],
         use_handler_docstrings=True,
         root_schema_site="swagger",
         enabled_endpoints={"swagger", "openapi.json"},
