14bb0dbfcca0e77ebe13e9b27dce056e0d5147e7
yezi <yehaowei20060411@qq.com>
2024-02-04 21:16:52 +0800

    feat: 移除旧版数据获取相关函数

--- frontend/src/utils/errorHandler.ts
+++ frontend/src/utils/errorHandler.ts
@@ -1,49 +1,4 @@
 import { toastError, toastWarning } from "@sscreator/ui";
-import type { ErrorCallbackArgs } from "./sendRequest";
-
-export function commonAPIErrorHandler({
-  httpCode,
-  httpMsg,
-  ApiCode,
-  ApiMsg,
-  error,
-}: ErrorCallbackArgs) {
-  // 请求超时
-  if (error?.name === "AbortError") {
-    toastError({
-      message: "请求超时，请重试或更换网络环境",
-    });
-    return;
-  }
-
-  // 其它阻止请求正常发送的异常
-  if (error && !httpCode && !ApiCode) {
-    toastError({
-      message: "网络异常，请重试或更换网络环境",
-    });
-    return;
-  }
-
-  // BAD ARGUMENTS，一般是用户传入参数无效导致
-  if (ApiCode && ApiMsg && ApiCode === 203) {
-    toastWarning({
-      message: ApiMsg,
-    });
-    return;
-  }
-
-  if (ApiCode && ApiMsg) {
-    toastError({
-      message: `${ApiMsg}\n（HTTP ${httpCode} / API ${ApiCode}）`,
-    });
-  }
-
-  if (httpCode && httpMsg && !ApiCode && !ApiMsg) {
-    toastError({
-      message: `未知错误\n${httpMsg}（HTTP ${httpCode}）`,
-    });
-  }
-}
 
 export function onError(error: Error) {
   // BAD ARGUMENTS，一般是用户传入参数无效导致
