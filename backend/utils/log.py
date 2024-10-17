9396dc8aae9d12784992becb77fc054b8655bf00
FHU-yezi <yehaowei20060411@qq.com>
2024-10-17 16:55:24 +0800

    feat: 日志记录切换到 `sshared`

--- backend/utils/log.py
+++ backend/utils/log.py
@@ -1,12 +1,10 @@
-from pymongo import MongoClient
 from sshared.logging import Logger
 
 from utils.config import CONFIG
 
-_client = MongoClient()
-
 logger = Logger(
     display_level=CONFIG.logging.display_level,
     save_level=CONFIG.logging.save_level,
-    save_collection=_client[CONFIG.mongo.database].log,
+    connection_string=CONFIG.postgres.logging_connection_string,
+    table=CONFIG.logging.table,
 )
